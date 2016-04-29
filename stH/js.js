2b75511816a9d3c388f76741c11c9ddaa109c9dd
MatyashN <it.is.my.cool@gmail.com>
2016-04-29 15:25:48 +0300

    commit

--- stH/js.js
+++ stH/js.js
@@ -1,155 +1,69 @@
 
-window.addEventListener("load",function() {
-            setTimeout(function(){
-                window.scrollTo(0, 1);
-            }, 0);
-        });
+    var el = document.getElementById('header');
+    var top_offset = _getCoords(el).top;
+        
+    el.style.top = '';
 
-try {
-        var el = $('#header');
-        var top_offset = $('#wrapper').offset().top;
-    } catch (e) { }
-
-    $(window).load(function(e){
-        try {
-            el.css('top', '');
-            top_offset = $('#wrapper').offset().top;
-        } catch (e) { }
-        $(window).scroll(function () {
-            var scroll_top = $(window).scrollTop();
-
-            if (scroll_top > top_offset) {
-                el.css('top', scroll_top - top_offset);
-            }
-            else {
-                el.css('top', '');
-            }
-        });
-    });
-
-
-    // for iphone SETTING THE HEIGHT OF THE FEATURE'S MAIN IMAGE SO THE STICKY CAN DETECT THE BOTTOM AND IT WILL STICK TO IT DEPENDING ON THE SCREEN SIZE AND OREINTATION ------------------------------
-
-    detectOrientation();
-    window.onorientationchange = detectOrientation;
-    function detectOrientation() {
-
-        function dett() {
-            var el = $('#header'),
-              top_offset = $('#wrapper').offset().top;
-            $(window).scroll(function () {
-                var scroll_top = $(window).scrollTop();
-
-                if (scroll_top > top_offset) {
-                    el.css('top', scroll_top - top_offset);
-                }
-                else {
-                    el.css('top', '');
-                }
-            });
-        }
-
-
-        if (typeof window.onorientationchange != 'undefined') {
-            var orientation = detectOrientation;
-            if (orientation == 0) {
-                $(".inv_main_image img").height(200);
-                $(function () {
-                    dett();
-                });
-
-            }
-            else if (orientation == 90) {
-                $(".inv_main_image img").height(280);
-                $(function () {
-                    dett();
-                })
-            }
-
-            else if (orientation == -90) {
-                $(".inv_main_image img").height(280);
-                var el = $('#header'),
-                top_offset = $('#wrapper').offset().top;
-
-                $(function () {
-                    dett();
-                })
-            }
-            else if (orientation == 180) {
-                $(".inv_main_image img").height(200);
-            }
+    function _getPosition(elem){
+        var scrollTop = _getCoords(elem).scrollTop;
+        if (scrollTop > top_offset) {
+            elem.style.top = scrollTop - top_offset - (startPoint.y - endPoint.y) + "px";
+        } else {
+            elem.style.top = '';
         }
     }
 
+    var startTouchMove, nowPoint, startPoint, endPoint;
 
-    // for Android - SETTING THE HEIGHT OF THE FEATURE'S MAIN IMAGE SO THE STICKY CAN DETECT THE BOTTOM AND IT WILL STICK TO IT DEPENDING ON THE SCREEN SIZE AND OREINTATION ------------------------------
-
-
-
-    var isAndroid = navigator.userAgent.toLowerCase().indexOf("android");
-
-    if (isAndroid > -1) {
-
-
-        detectOrientation2();
-        window.onorientationchange = detectOrientation2;
-        function detectOrientation2() {
-
-            function dett() {
-                var el = $('#header'),
-                  top_offset = $('#wrapper').offset().top;
-                $(window).scroll(function () {
-                    var scroll_top = $(window).scrollTop();
-
-                    if (scroll_top > top_offset) {
-                        el.css('top', scroll_top - top_offset);
-                    }
-                    else {
-                        el.css('top', '');
-                    }
-                });
-            }
-
-
-            if (typeof window.onorientationchange != 'undefined') {
-                var orientation = detectOrientation2;
-                if (orientation == 0) {
-                    $('.ui-body-a .ui-link img').width(104);
-                    $('.ui-body-a .ui-link img').height('auto');
-                    $(".arrowCont").css({ top: '50px' });
-                    $(".inv_main_image img").height(220);
-
-                    $(function () {
-                        dett();
-                    });
-
-                }
-                else if (orientation == 90) {
-                    $(".arrowCont").css({ top: '100px' });
-                    $(".inv_main_image img").height(380);
-
-                    $(function () {
-                        dett();
-                    })
-                }
-
-                else if (orientation == -90) {
-                    $(".arrowCont").css({ top: '100px' });
-                    $(".inv_main_image img").height(380);
-                    var el = $('#header'),
-                    top_offset = $('#wrapper').offset().top;
-
-                    $(function () {
-                        dett();
-                    })
-                }
-                else if (orientation == 180) {
-                    $('.ui-body-a .ui-link img').width(104);
-                    $('.ui-body-a .ui-link img').height('auto');
-                    $(".inv_main_image").height(220);
-                }
-            }
+    function touchStartHandler(event, elem){
+        
+        if (_getCoords().scrollTop < 0){
+            startPoint.x = event.changedTouches[0].clientX;
+            startPoint.y = event.changedTouches[0].clientY;
         }
+        
+
+    };
+
+    function touchEndHandler(event){
+
+        if (_getCoords().scrollTop < 0) {
+            endPoint.x = event.changedTouches[0].clientX;
+            endPoint.y = event.changedTouches[0].clientY;
+        } 
+
+    };
 
 
-    }
+
+
+    window.addEventListener('scroll', function(){_getPosition(el)}, false)
+    window.removeEventListener('touchstart', touchStartHandler, false)
+    window.addEventListener('touchend', touchEndHandler, false)
+
+    function _getCoords(elem) {
+        if (!elem) elem = document.documentElement;
+
+        var box = elem.getBoundingClientRect();
+
+        var body = document.body;
+        var docEl = document.documentElement;
+
+        var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+        var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+        var clientTop = docEl.clientTop || body.clientTop || 0;
+        var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+        var top = box.top + scrollTop - clientTop;
+        var left = box.left + scrollLeft - clientLeft;
+
+        return {
+            top: top,
+            left: left,
+            scrollTop: scrollTop - clientTop,
+            scrollLeft: scrollLeft - clientLeft
+        };
+    };
+
+
