5a05d10d1a90bf63accf4eff3703b35275365ff6
MatyashN <it.is.my.cool@gmail.com>
2016-04-29 14:13:42 +0300

    commit

--- stH/js.js
+++ stH/js.js
@@ -1,5 +1,9 @@
 
-
+window.addEventListener("load",function() {
+            setTimeout(function(){
+                window.scrollTo(0, 1);
+            }, 0);
+        });
 
 try {
         var el = $('#header');
