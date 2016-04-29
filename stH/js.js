14093e3a3f2fbf1fc3a6e4bc60b411c0a06288d8
MatyashN <it.is.my.cool@gmail.com>
2016-04-29 15:29:08 +0300

    commit

--- stH/js.js
+++ stH/js.js
@@ -14,6 +14,15 @@
     }
 
     var startTouchMove, nowPoint, startPoint, endPoint;
+    startPoint = {
+        x:0,
+        y:0
+    }
+    endPoint = {
+        x:0,
+        y:0
+    }
+
 
     function touchStartHandler(event, elem){
         
